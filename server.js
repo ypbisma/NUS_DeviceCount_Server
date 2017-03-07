308b85bd1cb50bb71f5daccd55cd28b301c1ec02
BisMac <Bisma@Yohaness-MacBook-Pro.local>
2017-03-08 01:43:12 +0800

    added server functionalities

--- server.js
+++ server.js
@@ -9,9 +9,65 @@ var db = new sqlite3.Database(file);
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080;	//set our port
+var port = process.env.PORT || 9090;	//set our port
 
 //	ROUTES FOR OUR API
 // ===============
 
+var router = express.Router();
+
+// test route to make sure everything is working
+
+router.get('/devicecountzone', function(req, res) {
+	
+
+	db.serialize(function() {
+		var zoneDeviceCountList = [];
+		db.all("Select rowid AS id, zoneId, zoneName, deviceCount, time FROM AggregateZone", function(err, rows) {
+			for (var i = 0; i < rows.length; i++) {
+				zoneDeviceCountList.push({id: rows[i].id, zoneId: rows[i].zoneId, zoneName: rows[i].zoneName, deviceCount: rows[i].deviceCount, time: rows[i].time});
+			}		
+			
+			res.json({zone_counts: zoneDeviceCountList});
+		});
+	});
+});
+
+router.get('/devicecountbuilding', function(req, res) {
+	
+
+	db.serialize(function() {
+		var buildingDeviceCountList = [];
+		db.all("Select rowid AS id, buildingId, buildingName, deviceCount, time FROM AggregateBuilding", function(err, rows) {
+			for (var i = 0; i < rows.length; i++) {
+				buildingDeviceCountList.push({id: rows[i].id, buildingId: rows[i].buildingId, buildingName: rows[i].buildingName, deviceCount: rows[i].deviceCount, time: rows[i].time});
+			}		
+			
+			res.json({building_counts: buildingDeviceCountList});
+		});
+	});
+});
+
+router.get('/devicecountuni', function(req, res) {
+	
+
+	db.serialize(function() {
+		var uniDeviceCountList = [];
+		db.all("Select rowid AS id, uniId, uniName, deviceCount, time FROM AggregateUniversity", function(err, rows) {
+			for (var i = 0; i < rows.length; i++) {
+				uniDeviceCountList.push({id: rows[i].id, uniId: rows[i].uniId, uniName: rows[i].uniName, deviceCount: rows[i].deviceCount, time: rows[i].time});
+			}		
+			
+			res.json({uni_counts: uniDeviceCountList});
+		});
+	});
+});
+
+//Register Our Routes
+//all of our routes will be prefixed with /api
+app.use('/nusdcapi', router);
+
+//Start the server
+app.listen(port);
+console.log('Magic happens on port ' + port);
 
